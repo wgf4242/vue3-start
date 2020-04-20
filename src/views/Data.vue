c4fec99ba36eca7b6253be437bcd32fd87a51d45
wgf4242 <wgf4242@gmail.com>
2020-04-21 00:04:28 +0800

    Init

---
+++ src/views/Data.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Reactive</h1>
+
+    <div>count: {{state.count}}</div>
+
+    <div>double: {{state.double}}</div>
+    <br />
+    <button @click="increment">inc</button>
+    <hr />
+
+    <h1>Ref</h1>
+    <div>x: {{x}}</div>
+  </div>
+</template>
+
+<script>
+import { reactive, computed, ref } from "vue";
+export default {
+  name: "App",
+  components: {
+    // HelloWorld
+  },
+
+  setup() {
+    const x = ref(0);
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    });
+
+    function increment() {
+      state.count++;
+    }
+
+    return { x, state, increment };
+  }
+};
+</script>
+
+<style>
+</style>
