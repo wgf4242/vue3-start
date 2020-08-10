7cee7ff34e3528e4a1c6f63d6b749e68bea2b20d
wgf4242 <wgf4242@gmail.com>
2020-08-10 22:38:02 +0800

    js svg init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <svg-icon icon-class="note" class-name="card-panel-icon" />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
