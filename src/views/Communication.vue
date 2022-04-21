a005e5b6758a451ee1c36ef67d49325a3f0af789
wgf4242 <wgf4242@gmail.com>
2022-04-21 21:11:00 +0800

    sync

--- src/views/Communication.vue
+++ src/views/Communication.vue
@@ -15,6 +15,31 @@
     <hr>
     <div>Comp5: provide/inject</div>
     <comp5></comp5>
+    <div style="color: red;">Comp6: $attrs - not implement</div>
+
+    <h3>常用方式以下8种</h3>
+    <ul>
+      <li>props</li>
+      <li>$emit/ <span class="delete">$on</span></li>
+      <li><span class="delete">X$children</span>/$parent </li>
+      <li>$attrs/ <span class="delete">$listeners</span></li>
+      <li>ref</li>
+      <li>$root</li>
+      <li>eventbus</li>
+      <li>vuex/pinia</li>
+    </ul>
+    <ul>
+      <li>父子组件</li>
+      <li>props/$emit/$parent/ref/$attrs</li>
+    </ul>
+    <ul>
+      <li>兄弟组件</li>
+      <li>$parent/$root/eventbus/vuex</li>
+    </ul>
+    <ul>
+      <li>跨层级关第</li>
+      <li>eventbus/vuex/provide+inject</li>
+    </ul>
   </div>
 </template>
 
@@ -25,3 +50,9 @@ import comp3 from "./communication/comp3.vue"
 import comp4 from "./communication/comp4.vue"
 import comp5 from "./communication/comp5.vue"
 </script>
+
+<style>
+.delete {
+  text-decoration: line-through;
+}
+</style>
